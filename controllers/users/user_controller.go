007eca3dd50ba3dc994a6b8f13cc8d6649eb9062
alanyukeroo@gmail.com <alanyukeroo@gmail.com>
2021-09-23 00:24:23 +0700

    replacing iotill Reading all with ShouldBindJSON

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,36 +1,44 @@
 package users
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/alanyukeroo/bookstore_users-api/domain/users"
+	"github.com/alanyukeroo/bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	fmt.Println(user)
 
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	fmt.Println(string(bytes))
-
-	if err != nil {
-		//TODO: Handle error
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println(err)
 		return
 	}
-	fmt.Println(err)
+	// Bisa digantikan ShouldBindJSON
+	// bytes, err := ioutil.ReadAll(c.Request.Body)
+	// fmt.Println(string(bytes))
 
-	if err := json.Unmarshal(bytes, &user); err != nil {
-		// TODO :Handle json error
-		return
-	}
+	// if err != nil {
+	// 	//TODO: Handle error
+	// 	return
+	// }
+
+	// if err := json.Unmarshal(bytes, &user); err != nil {
+	// 	// TODO :Handle json error
+	// 	return
+	// }
 
 	fmt.Println(user)
 
-	c.String(http.StatusNotImplemented, "Ini ada POST Create User")
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		// TODO: Handle user creation error
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func SearchUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/alanyukeroo/bookstore_users-api/domain/users"
+)
+
+func CreateUser(user users.User) (*users.User, error) {
+
+	return &user, nil
+}
